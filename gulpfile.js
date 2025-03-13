f2863603707f68583c57b22ad3cc322e851609fc
Bogdan <herecomesthefog@gmail.com>
2025-03-13 14:30:46 +0500

    add purgecss & prettify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const { src, dest, parallel, watch } = require('gulp');
+const { src, dest, parallel, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const nunjucksRender = require('gulp-nunjucks-render');
 const fs = require('node:fs');
 const data = require('gulp-data');
+const purgeCSS = require('gulp-purgecss'); //чистка цсс от неиспользуемых в разметке классов
+const prettify = require('gulp-prettify'); //форматирование итогового хтмл для читаемости
 
 function styles() {
     return src(`./src/styles/styles.scss`, {sourcemaps: true})
@@ -12,6 +14,14 @@ function styles() {
         }))
         .pipe(dest(`./dist/styles`, {sourcemaps: true}))
         .pipe(browserSync.stream());
+}
+
+function purgecss() {
+    return src(`./dist/styles/styles.css`)
+        .pipe(purgeCSS({
+            content: ['./dist/*.html']
+        }))
+        .pipe(dest(`./dist/styles`))
 }
 
 function html() {
@@ -23,6 +33,14 @@ function html() {
         .pipe(nunjucksRender({
             path: "./src/pages/blocks/" //можно и просто строку
         }))
+        .pipe(prettify(
+            {
+                indent_size: 4,
+                indent_char: ' ',
+                inline: [], // перенос строки для всех инлайн элементов
+                end_with_newline: true // перенос строки в конце файла
+            }
+        ))
         .pipe(dest(`./dist/`))
         .pipe(browserSync.stream());
 }
@@ -67,6 +85,7 @@ exports.styles = styles
 exports.html = html
 exports.images = images
 exports.fonts = fonts
+exports.purgecss = purgecss
 
 //выполнение сценария по умолчанию
 exports.dev = parallel(styles, scripts, images, fonts, html, browsersync, watcher);
